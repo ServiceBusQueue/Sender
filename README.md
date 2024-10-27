2a66a6f24fad29a5f076bb9976dec9d7f34d5314
KALEEM IQBAL <kaleem5090@outlook.com>
2024-10-27 22:48:11 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Azure Service Bus Console Application
+
+This project contains a .NET console application that sends messages to an Azure Service Bus queue.
+
+## Prerequisites
+
+- [.NET 8 SDK](https://dotnet.microsoft.com/download/dotnet/8.0) installed on your machine.
+- An Azure subscription with access to Azure Service Bus.
+
+## Setup
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/ServiceBusQueue/Sender.git
+   cd Sender
---
+++ ServiceBusQueueConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Messaging.ServiceBus;
+class Program
+{
+    // Create a Service Bus client
+    private const string connectionString = "<ConnectioonString />";
+    private const string queueName = "<QueuName />";
+
+    static async Task Main(string[] args)
+    {
+        await SendMessagesAsync();
+    }
+
+    static async Task SendMessagesAsync()
+    {
+        // Create a Service Bus client
+        await using var client = new ServiceBusClient(connectionString);
+        // Create a sender for the queue
+        ServiceBusSender sender = client.CreateSender(queueName);
+
+        try
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Create a message
+                ServiceBusMessage message = new ServiceBusMessage($"Message {i}");
+                // Send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent: Message {i}");
+            }
+        }
+        finally
+        {
+            // Dispose the sender
+            await sender.DisposeAsync();
+        }
+    }
+}
